cc3bdff4e5f5f6ba9ad430f6ed9afdb54903d550
Антон Шмидт <schmidt.ajax@gmail.com>
2022-03-22 19:32:34 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -5,10 +5,68 @@
 using namespace std;
 
 class Solution {
+  struct Pos {
+    int row;
+    int column;
+  };
+
  public:
   int numRookCaptures(vector<vector<char>> &board) {
-    // FIXME
-    return 0;
+    Pos rook{-1, -1};
+    const int rowCount = board.size();
+    const int columnCount = board.front().size();
+    bool found = false;
+    for (int r = 0; r < rowCount && !found; ++r) {
+      for (int c = 0; c < columnCount && !found; ++c) {
+        if ('R' == board[r][c]) {
+          rook = {r, c};
+          found = true;
+        }
+      }
+    }
+
+    int count = 0;
+    for (int c = rook.column - 1; 0 <= c; --c) {
+      char f = board[rook.row][c];
+      if ('p' == f) {
+        ++count;
+        break;
+      } else if ('B' == f) {
+        break;
+      }
+    }
+
+    for (int c = rook.column + 1; c < columnCount; ++c) {
+      char f = board[rook.row][c];
+      if ('p' == f) {
+        ++count;
+        break;
+      } else if ('B' == f) {
+        break;
+      }
+    }
+
+    for (int r = rook.row - 1; 0 <= r; --r) {
+      char f = board[r][rook.column];
+      if ('p' == f) {
+        ++count;
+        break;
+      } else if ('B' == f) {
+        break;
+      }
+    }
+
+    for (int r = rook.row + 1; r < rowCount; ++r) {
+      char f = board[r][rook.column];
+      if ('p' == f) {
+        ++count;
+        break;
+      } else if ('B' == f) {
+        break;
+      }
+    }
+
+    return count;
   }
 };
 
